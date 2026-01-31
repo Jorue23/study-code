cc3df655f38f9f43680cf29c7f56d131e7d2fab2
Jorue <jost.ruething@gmail.com>
2026-01-31 16:23:16 +0100

    finish extendedChessBoard

--- courses/coding-in-C/Lab_2/extendedChessBoard.c
+++ courses/coding-in-C/Lab_2/extendedChessBoard.c
@@ -1,26 +1,43 @@
 #include<stdio.h>
 #include<string.h>
 
-int drawBetweenLines();
+int drawLetterRows();
+int drawBetweenLine();
+int drawFields(int lineNumber);
 
-char LETTER_ROWS[] = "ABCDEFGH";
 int MAX_LINES = 8;
+int currentLine;
+char LETTER_ROWS[] = "ABCDEFGH";
+char WHITE_FIELD[] = "|   ";
+char BLACK_FIELD[] = "|###";
 
 int main()
 {  
-    printf(" ");
+    drawLetterRows();
+    drawBetweenLine();
+    for(currentLine = 0; currentLine < MAX_LINES; currentLine++)
+    {
+        drawFields(currentLine);
+        drawBetweenLine();
+    }
+    drawLetterRows();
+    return 0;
+}
+
+int drawLetterRows()
+{
+    printf("   ");
     for(int i = 0; i < strlen(LETTER_ROWS); i++)
     {
         printf("   %c", LETTER_ROWS[i]);
     }
     printf("\n");
-    drawBetweenLines();
     return 0;
 }
 
-int drawBetweenLines()
+int drawBetweenLine()
 {
-    printf("  ");
+    printf("    ");
     for(int plus = 0; plus < strlen(LETTER_ROWS)+1; plus++)
     {
         printf("+");
@@ -29,7 +46,35 @@ int drawBetweenLines()
             printf("-");
         }
     }
-    printf("  ");
+    printf("  \n");
     return 0;
+}
 
+int drawFields(int lineNumber)
+{
+    printf("  ");
+    printf("%d ", 8 - lineNumber);
+    if(lineNumber%2 == 0)
+    {
+
+        for(int o = 0; o < 4; o++)
+        {
+            printf("%s", BLACK_FIELD);
+            printf("%s", WHITE_FIELD);
+
+        }
+        printf("|");
+    }
+    else
+    {
+        for(int k = 0; k < 4; k++)
+        {
+            printf("%s", WHITE_FIELD);
+            printf("%s", BLACK_FIELD);
+        }
+        printf("|");
+        
+    }
+    printf(" %d\n", 8 - currentLine);
+    return 0;
 }

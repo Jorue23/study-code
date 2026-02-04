c8f79aee9d5b85f0598d9a014487e57ec576f5fa
Jorue <jost.ruething@gmail.com>
2026-02-04 09:20:30 +0100

    Simplify and cleanup calculator

--- courses/coding-in-C/Lab_3/simpleCalculator.c
+++ courses/coding-in-C/Lab_3/simpleCalculator.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 
-void promptUserFloat(int position);
+int promptUserFloat(int position);
 void promptUserOperation();
 
 float first = 0.00;
@@ -13,73 +13,60 @@ int main()
     promptUserFloat(1);
     promptUserFloat(2);
     promptUserOperation();
+    while(getchar() != '\n' && getchar() != EOF);
+    getchar();
     return 0;
 }
 
-void promptUserFloat(int position)
+int promptUserFloat(int position)
 {
     if(position == 1)
     {
         printf("Please enter a floating point number(e.g. 3.00). ");
-        int result = scanf("%f", &first);
-        if(result != 1) 
+        if(scanf("%f", &first) != 1)
         {
-            printf("Invalid input try again!\n");
-            while(getchar() != '\n');
-            promptUserFloat(1);
+            printf("Invalid input!\n");
+            return 1;
         }
-        while(getchar() != '\n');
     }
     else
     {
-        printf("Please enter a floating point number(e.g. 3.00). ");
-        int result = scanf("%f", &second);
-        if(result != 1) 
+        printf("Please enter another floating point number(e.g. 3.00). ");
+        if(scanf("%f", &second) != 1)
         {
-            printf("Invalid input try again!\n");
-            while(getchar() != '\n');
-            promptUserFloat(2);
+            printf("Invalid input!\n");
+            return 1;
         }
-        while(getchar() != '\n');
     }
 }
 
 void promptUserOperation()
-    {
-        printf("Please select the operation. ");
-        scanf(" %c", &operation);
-        if(getchar() != '\n' || operation == '\n' || operation == ' ' || operation == '\t')
-        {
-            printf("Invalid input please try again!");
-            while(getchar() != '\n');
-            promptUserOperation();
-        }
-        else
-        {
-            switch(operation)
-            {   
-                case '+':
-                    printf("%f %c %f %c %f", first, operation, second, '=', first + second);
-                    break;
-                case '-':
-                    printf("%f %c %f %c %f", first, operation, second, '=', first - second);
-                    break;
-                case '*':
-                    printf("%f %c %f %c %f", first, operation, second, '=', first * second);
-                    break;
-                case '/':
-                    if(second == 0.0)
-                    {
-                        printf("Tried to divide by zero. Please enter a different operation or restart the program.");
-                    }
-                    else
-                    {
-                        printf("%f %c %f %c %f", first, operation, second, '=', first / second);
-                        break;
-                    }
-                default:
-                    printf("Invalid input please enter a valid operation (+, -, *, /).");
-                    break;
+{
+    printf("Please select the operation. ");
+    scanf(" %c", &operation);
+    switch(operation)
+    {   
+        case '+':
+            printf("%.2f %c %.2f %c %.2f", first, operation, second, '=', first + second);
+            break;
+        case '-':
+            printf("%.2f %c %.2f %c %.2f", first, operation, second, '=', first - second);
+            break;
+        case '*':
+            printf("%.2f %c %.2f %c %.2f", first, operation, second, '=', first * second);
+            break;
+        case '/':
+            if(second == 0.0)
+            {
+                printf("Error: Tried to divide by zero.");
             }
-        }
+            else
+            {
+                printf("%.2f %c %.2f %c %.2f", first, operation, second, '=', first / second);
+                break;
+            }
+        default:
+            printf("Invalid input please enter a valid operation (+, -, *, /).");
+            break;
     }
+}

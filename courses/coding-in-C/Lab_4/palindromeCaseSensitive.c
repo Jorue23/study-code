37a1b761cc6366df63409ce848086c6fbcc6a417
Jorue <jost.ruething@gmail.com>
2026-02-11 12:11:18 +0100

    clean up palindrome

--- courses/coding-in-C/Lab_4/palindromeCaseSensitive.c
+++ courses/coding-in-C/Lab_4/palindromeCaseSensitive.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<string.h>
-
+#include<math.h>
 
 
 int main()
@@ -9,11 +9,13 @@ int main()
     char *p_return = "is a palindrome";
 
     printf("Please enter a word that's shorter then 100 characters. The app will check if it is a palindrome!\n");
-    if(scanf("%99s", &userInput) != 1)
+    if (fgets(userInput, sizeof(userInput), stdin) == NULL)
     {
-        printf("Invalid Input!\n");
-        return 1;
+        printf("Invalid input.\n");
+        return -1;
     }
+    userInput[strcspn(userInput, "\n")] = '\0';
+
 
     int palLen = strlen(userInput);
     for(int i = 0; i < floor(palLen/2); i++)

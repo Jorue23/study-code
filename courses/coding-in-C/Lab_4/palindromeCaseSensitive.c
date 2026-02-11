566250e16b6a76653a851a07ae4453ba07f1dba6
Jorue <jost.ruething@gmail.com>
2026-02-11 12:04:11 +0100

    Do Lab 4

---
+++ courses/coding-in-C/Lab_4/palindromeCaseSensitive.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+
+
+int main()
+{
+    char userInput[100] = "";
+    char *p_return = "is a palindrome";
+
+    printf("Please enter a word that's shorter then 100 characters. The app will check if it is a palindrome!\n");
+    if(scanf("%99s", &userInput) != 1)
+    {
+        printf("Invalid Input!\n");
+        return 1;
+    }
+
+    int palLen = strlen(userInput);
+    for(int i = 0; i < floor(palLen/2); i++)
+    {
+        if(userInput[i] != userInput[palLen - i - 1])
+        {
+            p_return = "is not a palindrome";
+            break;
+        }
+    }
+    
+    printf("Your provided word \"%s\" %s\n", userInput, p_return);
+
+    return 0;
+}

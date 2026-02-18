ea851366b421426444efe51285f58b7a63693921
Jorue <jost.ruething@gmail.com>
2026-02-18 18:58:59 +0100

    start work on lab 7, first working things

---
+++ courses/coding-in-C/Lab_7/playlist.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Song
+{
+    char *title;
+    char *artist;
+
+    struct Song *p_next;
+} Song;
+
+typedef struct Playlist
+{
+    Song *p_first;
+} Playlist;
+
+void init_playlist(Playlist *p_playlist) {
+    p_playlist->p_first = NULL;
+    return;
+}
+
+void addSong(Playlist *p_playlist, char *p_title, char *p_artist) {
+
+    Song *newSong = malloc(sizeof *newSong);
+
+    if(!newSong) {
+        printf("Memory failed to allocate");
+        return;
+        //nothing to free because nothing has been allocated
+    }
+
+    newSong->title = malloc(strlen(p_title)+1);
+    newSong->artist = malloc(strlen(p_artist)+1);
+
+    if(!newSong->artist || !newSong->title) {
+        printf("Failed to allocated memory!");
+        free(newSong->artist);
+        free(newSong->title);
+        free(newSong);
+        return;
+        //if this fails, also free newSong struct because it has to have been initialized by now
+    }
+    strcpy(newSong->title, p_title);
+    strcpy(newSong->artist, p_artist);
+
+    newSong->p_next = NULL;
+
+    if(!p_playlist->p_first) {
+        p_playlist->p_first = newSong;
+        return;
+    }
+
+    //cycle to the last song and append our song to the end of the list
+    Song *p_lastSong = p_playlist->p_first;
+    while(p_lastSong->p_next) {
+        p_lastSong = p_lastSong->p_next;
+    }
+    p_lastSong->p_next = newSong;
+}
+
+void print_playlist(Playlist *p_playlist) {
+    Song *p_currentSong = p_playlist->p_first;
+    while (p_currentSong->p_next)
+    {
+        printf("%s - %s\n", p_currentSong->artist, p_currentSong->title);
+        p_currentSong = p_currentSong->p_next;
+    }
+    printf("%s - %s\n", p_currentSong->artist, p_currentSong->title);
+    
+}
+
+
+int main() {
+
+    Playlist newPlaylist;
+    init_playlist(&newPlaylist);
+    addSong(&newPlaylist, "Crawling", "Linkin Park");
+    addSong(&newPlaylist, "Layla", "Eric Clapton");
+    addSong(&newPlaylist, "Esperanto", "Max Herre");
+
+    print_playlist(&newPlaylist);
+
+    return 0;
+}

22d6aa7d7a07abf054160c2b8acbb8a823f5195e
Jorue <jost.ruething@gmail.com>
2026-04-13 15:27:29 +0200

    Lab_2 Section 2

---
+++ courses/coding-in-C++/Lab_2/Notes.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class Note {
+    private:
+        string* text;
+    public:
+        Note(string new_text) {
+            text = new string;
+            *text = new_text;
+        }
+        ~Note() {
+            delete text;
+            text = nullptr;
+            cout << "Memory freed!" << endl;
+        }
+        void display();
+};
+
+void Note::display() {
+    cout << *text << endl;
+}
+
+int main() {
+    Note myNote1("1+1=3!");
+    Note myNote2 = myNote1;
+
+    myNote1.display();
+    myNote2.display();
+
+    return 0;
+
+}
+

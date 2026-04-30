a725f9bbdd330626878b104f9a67b710d4c18a70
Jorue <jost.ruething@gmail.com>
2026-04-30 03:10:24 +0200

    write header files for SearchQuery

--- courses/coding-in-C++/Lab_2/SearchEngine/SearchQuery.hpp
+++ courses/coding-in-C++/Lab_2/SearchEngine/SearchQuery.hpp
@@ -12,6 +12,64 @@
 
 class SearchQuery {
 
+    private:
+
+        static constexpr int MAX_RESULTS_DEFAULT = 4;
+        std::string query;
+        int max_results;
+
+    public:
+
+        /**
+         * @brief Default Constructor for the SearchQuery class.
+         * 
+         * Creates an empty query with the default amount of maximum results.
+         */
+        SearchQuery();
+
+        /**
+         * @brief Constructs a SearchQuery with initial values.
+         * 
+         * @param[in] query Text content the user is search for
+         * @param max_results Max number of results matching the search to be shown to the user.
+         */
+        SearchQuery(const std::string &query, int max_results);
+
+        /**
+         * @brief Returns the query of the SearchQuery object.
+         * 
+         * @return Query as a string
+         */
+        std::string getQuery() const;
+
+        /**
+         * @brief Set a new Query for the SearchQuery object.
+         * 
+         * @param[in] query the new query for the object.
+         */
+        void setQuery(const std::string &query);
+
+        /**
+         * @brief Returns the maxResults specified by the user.
+         * 
+         * @return maxResults as int. 
+         */
+        int getMaxResults() const;
+
+        /**
+         * @brief Set a new number of max results for the query.
+         * 
+         * @param[in] maxResults the new number of max results.
+         */
+        void setMaxResults(int maxResults);
+
+        /**
+         * @brief checks wether the current query is empty and if maxResults is greater then 0.
+         * 
+         * @return true if query is non empty or made of whitespaces and max results is greater then zero
+         * @return false if not
+         */
+        bool isValid() const;
 };
 
 

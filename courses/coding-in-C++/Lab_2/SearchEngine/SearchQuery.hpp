eaf841824e15bff6af925a10a491d3ce54d12005
Jorue <jost.ruething@gmail.com>
2026-04-30 02:54:10 +0200

    start work on query builder

---
+++ courses/coding-in-C++/Lab_2/SearchEngine/SearchQuery.hpp
@@ -0,0 +1,18 @@
+/**
+ * @file SearchQuery.hpp
+ * @brief Declaration of the SearchQuery class.
+ * This header file defines a query builder object to validate and handle user input.
+ * It has query and maximum number of results to be shown to the user.
+ */
+
+#ifndef SEARCH_QUERY_HPP
+#define SEARCH_QUERY_HPP
+
+#include <string>
+
+class SearchQuery {
+
+};
+
+
+#endif
